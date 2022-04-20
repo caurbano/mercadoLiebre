39c025e74ea819e234abf14b78e8d94aaefcfd3b
caurbano <carla_urbano@hotmail.com>
2022-04-20 20:09:02 -0300

    Cambios agregados

--- app.js
+++ app.js
@@ -11,6 +11,10 @@ app.listen(3000, () => {
     console.log('Servidor corriendo en el puerto 3000');
 });
 
+// app.listen(process.env.PORT || 3000, () => {
+//     console.log('Servidor corriendo en el puerto 3000');
+// });
+
 app.get('/', (req, res) => {
     res.sendFile(path.resolve(__dirname, './views/home.html'))
 });
